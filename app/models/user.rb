5ca5bb92a04736a0784fdf6351de9f7f7a65ebf5
Robert Harężlak <harezlak.robert7@gmail.com>
2017-03-24 20:47:27 +0100

    fixture for user & method to digest password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6}
   has_secure_password
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : Bcrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
