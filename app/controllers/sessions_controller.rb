c0f3c75c06b0556df68cabecd0e7e07416b6d32f
Robert Harężlak <harezlak.robert7@gmail.com>
2017-03-25 01:54:51 +0100

    still logged juzer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     
     if @user && @user.authenticate(params[:session][:password])
+      remember @user
       log_in @user
       redirect_to @user
     else
